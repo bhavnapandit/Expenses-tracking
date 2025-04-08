a69f3afff1c6fe977a60887582ddbcb907172216
bhavnapandit <pritybhavna2001@gmail.com>
2025-04-08 13:01:28 +0530

    fixing cors error

--- backend/api/server.js
+++ backend/api/server.js
@@ -11,6 +11,13 @@ dotenv.config();
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://expenses-tracking-frontend.vercel.app");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    next();
+});
 // Middleware
 app.use(cors({
     origin: 'https://expenses-tracking-frontend.vercel.app',
